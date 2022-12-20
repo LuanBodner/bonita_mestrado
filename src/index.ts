81a7716e807c6a3969b26041b9df54343ef3fe7a
Luan Bodner do Rosário <luan.rosario.bodner@gmail.com>
2022-12-20 15:54:22 -0300

    fix(index): adds cors middleware

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import { createConnection } from 'typeorm';
 import express from "express";
 import routes from './routes'
 
-const port = 9999;
+var cors = require('cors')
+
+const port = 3000;
 
 const app = express();
 
 createConnection();
 
+app.use(cors())
 app.use(express.json({limit: '500mb'}));
 app.use(express.urlencoded({limit: '500mb'}));
 
@@ -17,9 +20,3 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(port);
-
-
-
-
-
-
